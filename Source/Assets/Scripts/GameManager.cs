5cd2a223f96592cdf2f6047d63024fb37c4a1607
Zuhair <zuhair.ghias@hotmail.com>
2022-04-02 18:00:34 -0500

    game manager

--- Source/Assets/Scripts/GameManager.cs
+++ Source/Assets/Scripts/GameManager.cs
@@ -3,14 +3,54 @@ using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
 
+/// <summary>
+/// An instance of this class is meant to exist independant of what level is loaded
+/// 
+/// The manager will grab the necessary referenced on sceneLoaded.
+/// </summary>
 public class GameManager : MonoBehaviour
 {
-
-    private bool FirstEvidenceFound;
+    private SceneLoader SL;
+    private Door Door;
 
     private void Awake()
     {
         DontDestroyOnLoad(this);
+
+        SL = FindObjectOfType<SceneLoader>();
+        if (SL == null)
+        {
+            Debug.LogError("Scene loader missing from scene");
+        }
+
+        SceneManager.sceneLoaded += SceneLoadEvent;
+        SceneManager.sceneUnloaded += SceneUnloadEvent;
+
+    }
+
+    /// <summary>
+    /// Add any references ou want to find in the scene here since scenes will change throughout the game
+    /// </summary>
+    /// <param name="scene"></param>
+    /// <param name="mode"></param>
+    private void SceneLoadEvent(Scene scene, LoadSceneMode mode)
+    {
+        this.Door = FindObjectOfType<Door>();
+
+        if (!this.Door)
+        {
+            Debug.LogWarning("This scene does not have a door");
+        }
+
+    }
+
+    /// <summary>
+    /// Good idea to unreference anything set in the SceneLoadEvent
+    /// </summary>
+    /// <param name="scene"></param>
+    private void SceneUnloadEvent(Scene scene)
+    {
+        this.Door = null;
     }
 
     private void Start()
@@ -55,11 +95,18 @@ public class GameManager : MonoBehaviour
 
     private IEnumerator CrimeScene1()
     {
-        Debug.Log("CrimeScene1");
-        SceneManager.LoadScene("CrimeScene1");
+        yield return SL.ChangeScene("CrimeScene1");
+
+        Debug.Log("Loaded CrimeScene1");
 
         bool leaveScene = false;
-        //TODO: Add event delegate for leave event
+
+        if (!Door)
+        {
+            Debug.LogError("Door not found in scene");
+        }
+
+        Door.Leave += () => leaveScene = true;
 
         while (!leaveScene)
         {
@@ -71,8 +118,9 @@ public class GameManager : MonoBehaviour
 
     private IEnumerator Interrogation1()
     {
+        yield return SL.ChangeScene("Interrogation1");
         Debug.Log("Interrogation1");
-        SceneManager.LoadScene("GameLoopTest 1");
+        //SceneManager.LoadScene("Interrogation1");
 
         bool leaveInterrogation = false;
         //TODO: Add event delegate for leave event
