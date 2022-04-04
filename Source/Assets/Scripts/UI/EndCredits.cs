c0f1eaaa7543ed0554985b16eccb0ae7b0db348f
MahadAdnanGul <64431074+MahadAdnanGul@users.noreply.github.com>
2022-04-05 00:41:17 +0500

    Added End Credits

---
+++ Source/Assets/Scripts/UI/EndCredits.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class EndCredits : MonoBehaviour
+{
+    [SerializeField] private List<CanvasGroup> creditPanels;
+    private int currentIndex = -1;
+    private void Start()
+    {
+        RollCredits();
+    }
+
+    private void RollCredits()
+    {
+        currentIndex++;
+        if (currentIndex >= creditPanels.Count)
+        {
+            //Main Menu?
+            return;
+        }
+        creditPanels[currentIndex].DOFade(1, 4f).onComplete = () =>
+        {
+            creditPanels[currentIndex].DOFade(0, 4f).onComplete = RollCredits;
+        };
+    }
+}
