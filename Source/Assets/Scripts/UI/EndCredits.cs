c25eaf99f8af2501b69cb9c6433de6cd30915e46
Alexander Mastryukov <Alexandermastryukov@gmail.com>
2022-04-04 18:22:17 -0400

    Populate first alleyway scene

--- Source/Assets/Scripts/UI/EndCredits.cs
+++ Source/Assets/Scripts/UI/EndCredits.cs
@@ -8,6 +8,7 @@ public class EndCredits : MonoBehaviour
 {
     [SerializeField] private List<CanvasGroup> creditPanels;
     private int currentIndex = -1;
+
     private void Start()
     {
         RollCredits();
@@ -21,6 +22,7 @@ public class EndCredits : MonoBehaviour
             //Main Menu?
             return;
         }
+
         creditPanels[currentIndex].DOFade(1, 4f).onComplete = () =>
         {
             creditPanels[currentIndex].DOFade(0, 4f).onComplete = RollCredits;
